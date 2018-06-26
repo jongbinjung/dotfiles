c9863ca6b0e3a3220c4092805043b4312d6276e4
Jongbin Jung <jongbin.jung@salesforce.com>
2018-06-26 13:46:20 -0700

    Add git config script

---
+++ git_config.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Created: 2018-06-20
+# Author: jongbin.jung
+# Description: Default git configs
+
+git config --global user.name "Jongbin Jung"
+# TODO: Ask for email?
+git config --global user.email jungjongbin86@gmail.com
+
+git config --global blame.date short
