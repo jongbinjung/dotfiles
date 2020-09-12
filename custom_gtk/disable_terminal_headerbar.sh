dbb6bc1942cbf0c2d08980dabacdd8de88f7086b
Jongbin Jung <me@jongbin.com>
2020-09-12 14:45:47 -0700

    Add gnome-terminal color scheme for iceberg

---
+++ custom_gtk/disable_terminal_headerbar.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Created: 2019-06-11
+# Author: jongbin
+# Description: Disable Headerbar for Terminal in Gnome 3.32+
+sudo gsettings set org.gnome.Terminal.Legacy.Settings headerbar false
+
+
