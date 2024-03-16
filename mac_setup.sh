19e9b508ccc51f792c19800dc50f6045a9366f47
Jongbin Jung <jungjongbin86@gmail.com>
2024-03-16 10:02:32 -0700

    Add fzf, git_delta, and dprint to macOS setup

--- mac_setup.sh
+++ mac_setup.sh
@@ -7,3 +7,15 @@
 # X-style drag/resize:
 # https://github.com/dmarcotte/easy-move-resize
 brew cask install easy-move-plus-resize
+
+brew install fzf
+# To install useful key bindings and fuzzy completion:
+$(brew --prefix)/opt/fzf/install
+
+brew install git_delta
+
+# Markdown LSP (https://github.com/artempyanykh/marksman)
+brew install marksman
+
+# dprint for arbitrary code formatting
+brew install dprint
