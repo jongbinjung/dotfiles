9bd84db99231ddf8969761bace45d92e76e207b9
Jongbin Jung <jungjongbin86@gmail.com>
2024-03-16 10:02:21 -0700

    Update global git configs

--- git_config.sh
+++ git_config.sh
@@ -11,3 +11,18 @@ git config --global user.email jungjongbin86@gmail.com
 git config --global blame.date short
 
 git config --global format.pretty format:"%C(auto)%h%d %C(green)%aN%Creset %s"
+
+git config --global core.pager delta
+
+git config --global alias.tags "log --no-walk --tags --color --pretty='%C(auto)%h%d%Creset %s %C(blue)(%ad)'"
+
+git config --global log.date relative
+git config --global delta.navigate true
+git config --global delta.light false
+git config --global merge.conflictstyle diff3
+git config --global diff.colorMoved default
+
+# [includeIf "gitdir:~/repos/"]
+#   path = ~/repos/.gitconfig
+# [includeIf "gitdir:~/.vim/"]
+#   path = ~/repos/.gitconfig
