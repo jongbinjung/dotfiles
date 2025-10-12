642ebf857223a69c31b11185bad0b4c01cda3c10
Jongbin Jung <jungjongbin86@gmail.com>
2025-10-12 10:57:28 -0700

    Initial Fedora setup script

--- vim_config.sh
+++ vim_config.sh
@@ -1,9 +1,7 @@
-env LDFLAGS=-L$HOME/.local/lib ./configure --with-features=huge \
-       	--enable-multibyte \
-       	--enable-rubyinterp=yes \
-       	--enable-python3interp=yes \
-       	--with-python3-config-dir=$(python3.7-config --configdir) \
-       	--enable-perlinterp=yes \
-       	--enable-luainterp=yes \
-       	--enable-cscope \
-       	--prefix=$HOME/.local
+./configure \
+  --prefix=$HOME/.local \
+  --with-features=huge \
+  --enable-multibyte \
+  --enable-python3interp=dynamic \
+  --with-wayland=yes \
+  --enable-cscope
