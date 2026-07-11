0e0470404201d77b2c66fcc730090cb9ae1705c2
Jongbin Jung <jungjongbin86@gmail.com>
2026-07-10 18:39:40 -0700

    Add hyprland.lua

---
+++ .config/hypr/hyprland.lua
@@ -0,0 +1,32 @@
+--  vim: set ts=4 sw=4 tw=120 foldmethod=indent et :
+-- Refer to the wiki for more information.
+-- https://wiki.hypr.land/Configuring/Start/
+
+-- MONITORS
+require("hyprland.monitors")
+
+-- AUTOSTART
+require("hyprland.autostart")
+
+-- ENVIRONMENT VARIABLES
+require("hyprland.envvars")
+
+-- PERMISSIONS
+require("hyprland.permissions")
+
+
+-- LOOK AND FEEL
+require("hyprland.look_and_feel")
+
+
+-- MISC
+require("hyprland.misc")
+
+-- INPUT
+require("hyprland.input")
+
+-- KEYBINDINGS
+require("hyprland.keybindings")
+
+-- WINDOWS AND WORKSPACES
+require("hyprland.windowrules")
