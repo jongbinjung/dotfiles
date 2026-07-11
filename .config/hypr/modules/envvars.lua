18af9fe2b680fe346d733cc012daa2c044628368
Jongbin Jung <jungjongbin86@gmail.com>
2026-07-11 13:10:01 -0700

    More hyprland lua-fication

---
+++ .config/hypr/modules/envvars.lua
@@ -0,0 +1,19 @@
+-------------------------------
+---- ENVIRONMENT VARIABLES ----
+-------------------------------
+
+-- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
+hl.env("ELECTRON_OZONE_PLATFORM_HINT", "wayland")
+hl.env("GDK_SCALE", "1.25")
+hl.env("HYPRCURSOR_SIZE", "24")
+
+-- hl.env("QT_IM_MODULE", "ibus")
+-- hl.env("QT_IM_MODULES", "wayland;fcitx;ibus")
+hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
+hl.env("QT_QPA_PLATFORM", "wayland")
+
+-- toolkit-specific scale
+hl.env("GDK_SCALE", "2")
+hl.env("HYPRCURSOR_SIZE", "48")
+hl.env("XCURSOR_SIZE", "48")
+hl.env("XCURSOR_THEME", "Bibata-Modern-Classic")
---
+++ .config/hypr/modules/misc.lua
@@ -0,0 +1,8 @@
+hl.config({
+    misc = {
+        disable_hyprland_logo = true,
+        focus_on_activate = true,
+        force_default_wallpaper = -1,
+        key_press_enables_dpms = true,
+    },
+})
