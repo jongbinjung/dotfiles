2a8339386ab246d412208413ab594b351c56905a
Jongbin Jung <jungjongbin86@gmail.com>
2026-07-17 19:23:15 -0700

    [hyprland] Add envvars for qt combatible ibus

--- .config/hypr/modules/envvars.lua
+++ .config/hypr/modules/envvars.lua
@@ -7,8 +7,8 @@ hl.env("ELECTRON_OZONE_PLATFORM_HINT", "wayland")
 hl.env("GDK_SCALE", "1.25")
 hl.env("HYPRCURSOR_SIZE", "24")
 
--- hl.env("QT_IM_MODULE", "ibus")
--- hl.env("QT_IM_MODULES", "wayland;fcitx;ibus")
+hl.env("QT_IM_MODULE", "ibus")
+hl.env("QT_IM_MODULES", "wayland;fcitx;ibus")
 hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 hl.env("QT_QPA_PLATFORM", "wayland")
 
