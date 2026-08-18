58c135f177600722b263c44b91c9993ec5b1a249
Jongbin Jung <jungjongbin86@gmail.com>
2026-08-17 18:39:02 -0700

    Clean up zsh setup

--- scripts/setup_zsh.sh
+++ scripts/setup_zsh.sh
@@ -1,47 +1,5 @@
-#!/usr/bin/env zsh
-BASE_DIR=$PWD
+#!/usr/bin/env bash
+set -Eeuo pipefail
 
-# Create symlink for necessary dotfiles
-echo BASE_DIR=$BASE_DIR
-echo ZSH_CUSTOM=$ZSH_CUSTOM
-echo "Setup all aliases?"
-select yn in "Yes" "No"
-do
-  case $yn in
-    Yes )
-      ln -sf "$BASE_DIR/zsh/alias.zsh" "$ZSH_CUSTOM/alias.zsh"
-      ln -sf "$BASE_DIR/zsh/path.zsh" "$ZSH_CUSTOM/path.zsh"
-      ln -sf "$BASE_DIR/.p10k.zsh" "$HOME/.p10k.zsh"
-      ln -sf "$BASE_DIR/.taskrc" "$HOME/.taskrc"
-      ln -sf "$BASE_DIR/.Xmodmap" "$HOME/.Xmodmap"
-      ln -sf "$BASE_DIR/.tmux.conf" "$HOME/.tmux.conf"
-      ln -sf "$BASE_DIR/.Xresources" "$HOME/.Xresources"
-      ln -sf "$BASE_DIR/.flake8" "$HOME/.flake8"
-      ln -sf "$BASE_DIR/.pylintrc" "$HOME/.pylintrc"
-      break ;;
-    No)
-      exit ;;
-  esac
-done
-
-if [[ ! -d "$HOME/.tmux/plugins/tpm" ]]; then
-  git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
-  echo "Installed tmux plugin manager."
-  echo "Do prefix + I in tmux to initialize tmux plugins."
-fi
-
-echo "Setup pyenv?"
-select yn in "Yes" "No"
-do
-  case $yn in
-    Yes )
-      curl https://pyenv.run | bash
-      break ;;
-    No)
-      exit ;;
-  esac
-done
-
-echo "Restart your shell for all paths to take effect"
-
-unset BASE_DIR
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
+exec "$SCRIPT_DIR/bootstrap.sh" --no-packages "$@"
